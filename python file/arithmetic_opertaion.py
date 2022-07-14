1f0793865a892dc926116ae9dee21fec0446ce38
chain singh <chainrmsa3025@gmail.com>
2022-07-15 01:46:00 +0530

    arithmethic and variavle assignemt

---
+++ python file/arithmetic_opertaion.py
@@ -0,0 +1,7 @@
+print(1+1) #  2 addition
+print(2*4) # 8 multiplication
+print(1-1) # 0 subtraction
+print(4/2) # 2 divides
+
+print(2**3) # 8 power
+print(10%3)  # 1 mod
---
+++ python file/variable_assignment.py
@@ -0,0 +1,5 @@
+my_income = 35000
+tax_rate = 0.10
+
+my_tax = my_income*tax_rate
+print(my_tax)
